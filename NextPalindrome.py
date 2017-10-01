2200338690c167730444047657e4bc2088b5c873
yusuf287 <yusuf_sid@yahoo.com>
2017-10-02 00:58:45 +0530

    Add files via upload

---
+++ NextPalindrome.py
@@ -0,0 +1,19 @@
+#HOW TO FIND NEXT PALINDROME NUMBER
+
+print("Please provide any Palindrome number!!")
+num = input()
+
+l = len(num)
+
+n = l//2                    
+
+if(num == '9'):                     #Find middle position              
+    next = 11
+elif(l%2 != 0):                     #If numer is ODD
+    temp = str(int(num[:n+1])+1)
+    next = temp[:n] + temp[::-1]
+else:                               #If number is EVEN
+    temp = str(int(num[:n])+1)
+    next = temp[:n] + temp[::-1]
+    
+print("Next Palindrome is: ", next)
